ed628ddc105f2d456396481afaf06840e9425bd6
LordnistLost <lordnsmaus@gmail.com>
2024-12-04 22:37:10 +0100

    apply benchmark.template

--- 2024/src/lib.rs
+++ 2024/src/lib.rs
@@ -1,5 +1,8 @@
 #![feature(iter_map_windows)]
+#![cfg_attr(feature = "nightly", feature(test))]
 mod day;
 pub mod days;
 mod input;
 pub use day::Day;
+#[cfg(feature = "nightly")]
+extern crate test;
